335efc8a29ea27f9a0170b85a8aef6b20e6e39f8
Oleg Kushch <okushch@justcoded.co>
2017-12-11 13:26:09 +0200

    Fix cancellation logic

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -5,26 +5,32 @@ class TaskItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      newTaskTitle: ''
+      newTaskTitle: this.props.task.title
     }
+  }
+
+  cancel() {
+    this.setState((prevState, props) => {       
+      this.props.onCancel()
+      return {newTaskTitle: props.task.title}
+    });
   }
 
   render() {
     const selectedControls = (
       <div className="row">
         <div className="col-md-9 col-xs-9">
-          <input className="form-control" type="text" value={this.props.task.title} onChange={(e) => {
-            this.props.task.title = e.target.value
+          <input className="form-control" type="text" value={this.state.newTaskTitle} onChange={(e) => {
             this.setState({ newTaskTitle: e.target.value })
           }} />
         </div>
         <div className="col-md-3 col-xs-3">
           <div className="row">
             <div className="col-md-6 col-xs-6">
-              <button className="btn btn-default" onClick={()=>{this.props.onSave(this.state.newTaskTitle)}}>Save</button>
+              <button className="btn btn-default" onClick={() => { this.props.onSave(this.state.newTaskTitle) }}>Save</button>
             </div>
             <div className="col-md-6 col-xs-6">
-              <button className="btn btn-default" onClick={this.props.onCancel}>Cancel</button>
+              <button className="btn btn-default" onClick={this.cancel.bind(this)}>Cancel</button>
             </div>
           </div>
         </div>
@@ -47,7 +53,7 @@ class TaskItem extends Component {
       </div>);
 
     return (
-      <li className={ this.props.task.isCompleted ? 'list-group-item completed' : 'list-group-item' }>
+      <li className={this.props.task.isCompleted ? 'list-group-item completed' : 'list-group-item'}>
         {this.props.task.isEditable ? selectedControls : unSelectedControls}
       </li>
     );
